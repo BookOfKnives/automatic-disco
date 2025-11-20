b41b84e54eee9a5c91852b1900425f9498666122
hansen <hansen>
2025-11-20 22:05:34 +0100

    five faker users now online.

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,26 @@
 //2011 2025
 //21:43
 //lets make some code.
+console.log("appsvelte online frontend w009")
+import { faker } from '@faker-js/faker';
+function createRandomUser() {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    birthdate: faker.date.birthdate(),
+  };
+}
+
+let userArray = [createRandomUser(), createRandomUser(), createRandomUser(), createRandomUser(), createRandomUser()]
+
+// console.log("user?")
+console.log(userArray)
+
 </script>
 
 <main>
-
+  <p>Hello and welcome.</p>
 </main>
 
 <style>
